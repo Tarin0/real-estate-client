7457bfa8fbb19611670685a6f1f2cf8fbfa1670f
Tarin0 <tarinmostofa837@gmail.com>
2024-03-19 20:44:30 +0600

    reviewer post section and modal added for review posting

---
+++ src/Pages/Property/ReviewModal.jsx
@@ -0,0 +1,124 @@
+import toast from "react-hot-toast";
+import useAuth from '../../hooks/useAuth.js';
+
+const ReviewModal = ({ property }) => {
+    console.log(property);
+    const { user } = useAuth();
+    const { name, title, email } = property;
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const form = e.target;
+        const reviewerImage = user?.photoURL;
+        const reviewerEmail = user?.email;
+        const reviewerName = user?.displayName;
+        const agentName = name;
+        const agentEmail = email;
+        const reviewDescription = form.description.value;
+        const rating = form.rating.value;
+        const currentDate = new Date();
+
+        const dateString = currentDate.toLocaleDateString(); // Get date string
+        const timeString = currentDate.toLocaleTimeString(); // Get time string
+
+
+        // console.log(selectedSlot);
+        try {
+
+            const property = { reviewerName, agentName, reviewerEmail, agentEmail, reviewerImage, title, reviewDescription, rating, date: dateString, time: timeString, };
+            fetch('http://localhost:5000/reviews', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(property)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.insertedId) {
+                        toast.success('Your Review added');
+                        form.reset();
+                    }
+                })
+        }
+        catch (err) {
+            toast.error(err);
+        }
+
+    };
+
+    return (
+        <div className="my-12 grid justify-center">
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <button className="btn text-xl mt-5 px-6 py-2 border rounded-md bg-violet-400 text-gray-900 border-violet-400" onClick={() => document.getElementById('my_modal_5').showModal()}>Please Review</button>
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <div className="flex flex-col max-w-xl p-8 shadow-sm rounded-xl lg:p-12 bg-gray-900 text-gray-100">
+                        <div className="flex flex-col items-center w-full">
+                            <h2 className="text-3xl font-semibold text-center">Your opinion matters!</h2>
+                            <div className="flex flex-col items-center py-6 space-y-3">
+                                <span className="text-center">How was your experience?</span>
+                                <div className="flex space-x-3">
+                                    <button type="button" title="Rate 1 stars" aria-label="Rate 1 stars">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                        </svg>
+                                    </button>
+                                    <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                        </svg>
+                                    </button>
+                                    <button type="button" title="Rate 3 stars" aria-label="Rate 3 stars">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                        </svg>
+                                    </button>
+                                    <button type="button" title="Rate 4 stars" aria-label="Rate 4 stars">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                        </svg>
+                                    </button>
+                                    <button type="button" title="Rate 5 stars" aria-label="Rate 5 stars">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-gray-600">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="flex flex-col w-full">
+                                    <label className="text-white">Review</label>
+
+                                    <textarea rows="3" name='description' placeholder="write your review" className="border border-gray-200 p-4 rounded-md resize-none text-gray-100 bg-gray-900"></textarea>
+                                    <label className="text-white">Rating</label>
+                                    <input
+                                        name="rating"
+                                        type="text"
+                                        placeholder="Out of 5"
+                                        className="border border-gray-200 p-4 rounded-md resize-none text-gray-100 bg-gray-900"
+                                    />
+                                    <button type="submit" className="py-4 my-8 font-semibold rounded-md text-gray-900 bg-violet-400">Leave feedback</button>
+                                </div>
+                                <div>
+
+                                </div>
+                            </form>
+
+                        </div>
+
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Maybe Later</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
+};
+
+export default ReviewModal;
